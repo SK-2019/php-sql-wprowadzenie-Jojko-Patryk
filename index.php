40df911879e9300192e374b989991eeabfe04431
Jojko-Patryk <73522929+Jojko-Patryk@users.noreply.github.com>
2020-10-30 14:28:36 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
 
             echo("</table>");
             $conn = new mysqli("mysql-phanteon.alwaysdata.net","phanteon","hasło_testowe","phanteon_test_base");  
-        $result = $conn->query('SELECT * FROM pracownicy where zarobki>20'); 
+        $result = $conn->query('SELECT * FROM pracownicy where zarobki between 20 and 10'); 
             echo("<table border=1>");
             echo("<th>Id</th>"); 
             echo("<th>Imie</th>");
@@ -54,21 +54,7 @@
                 while($row=$result->fetch_assoc()){ 
                     echo("<tr>");
                         echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>"); 
-    
-                    echo("</tr>");
-                }
-                echo("</table>");
-                $conn = new mysqli("mysql-phanteon.alwaysdata.net","phanteon","hasło_testowe","phanteon_test_base");  
-            $result = $conn->query('SELECT * FROM pracownicy where (dzial=1 or dzial=3) and imie like "%a"'); 
-                echo("<table border=1>");
-                echo("<th>Id</th>"); 
-                echo("<th>Imie</th>");
-                echo("<th>Dzial</th>");
-                echo("<th>Zarobki</th>");
-                    while($row=$result->fetch_assoc()){ 
-                        echo("<tr>");
-                            echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>"); 
-        
                         echo("</tr>");
-                    }
-         ?>
+                }
+                ?>
+            
