42f78aca408e03bd003f95cd921de56d72b681d5
Jojko-Patryk <73522929+Jojko-Patryk@users.noreply.github.com>
2020-10-30 14:31:05 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,60 +1,54 @@
 <?php
-    $conn = new mysqli("mysql-phanteon.alwaysdata.net","phanteon","hasło_testowe","phanteon_test_base");  
+    $conn = new mysqli("mysql-phanteon.alwaysdata.net","phanteon","hasło_testowe","phanteon_test_base");  //Adres do którego się łączę, nazwa użytkownika, hasło, baza danych
     $result = $conn->query('SELECT * FROM pracownicy');
         echo("<table border=1>");
-        echo("<th>Id</th>"); 
+        echo("<th>Id</th>"); //Nazwy Kolumn
         echo("<th>Imie</th>");
         echo("<th>Dzial</th>");
         echo("<th>Zarobki</th>");
-            while($row=$result->fetch_assoc()){ 
+            while($row=$result->fetch_assoc()){ // wyciąganie danych VVVVVVV
                 echo("<tr>");
-                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>"); /
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>"); // wyciąga odpowiednie dane z bazy
 
                 echo("</tr>");
             }
 
-        echo("</table>");
-        $conn = new mysqli("mysql-phanteon.alwaysdata.net","phanteon","hasło_testowe","phanteon_test_base");  
-    $result = $conn->query('SELECT * FROM pracownicy where imie like "%a"'); 
-        echo("<table border=1>");
-        echo("<th>Id</th>"); 
-        echo("<th>Imie</th>");
-        echo("<th>Dzial</th>");
-        echo("<th>Zarobki</th>");
-            while($row=$result->fetch_assoc()){ 
-                echo("<tr>");
-                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>"); 
-
-                echo("</tr>");
-            }
-
-        echo("</table>");
-        $conn = new mysqli("mysql-phanteon.alwaysdata.net","phanteon","hasło_testowe","phanteon_test_base");  
-    $result = $conn->query('SELECT * FROM pracownicy where (dzial=1 or dzial=3)'); 
-        echo("<table border=1>");
-        echo("<th>Id</th>"); 
-        echo("<th>Imie</th>");
-        echo("<th>Dzial</th>");
-        echo("<th>Zarobki</th>");
-            while($row=$result->fetch_assoc()){ 
-                echo("<tr>");
-                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>"); 
-
-                echo("</tr>");
-            }
-
-            echo("</table>");
-            $conn = new mysqli("mysql-phanteon.alwaysdata.net","phanteon","hasło_testowe","phanteon_test_base");  
-        $result = $conn->query('SELECT * FROM pracownicy where zarobki between 20 and 10'); 
-            echo("<table border=1>");
-            echo("<th>Id</th>"); 
-            echo("<th>Imie</th>");
-            echo("<th>Dzial</th>");
-            echo("<th>Zarobki</th>");
-                while($row=$result->fetch_assoc()){ 
-                    echo("<tr>");
-                        echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>"); 
+            $conn = new mysqli("mysql-phanteon.alwaysdata.net","phanteon","hasło_testowe","phanteon_test_base");  //Adres do którego się łączę, nazwa użytkownika, hasło, baza danych
+            $result = $conn->query('SELECT * FROM pracownicy where (dzial=1 or dzial=3)');
+                echo("<table border=1>");
+                echo("<th>Id</th>"); //Nazwy Kolumn
+                echo("<th>Imie</th>");
+                echo("<th>Dzial</th>");
+                echo("<th>Zarobki</th>");
+                    while($row=$result->fetch_assoc()){ // wyciąganie danych VVVVVVV
+                        echo("<tr>");
+                            echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>"); // wyciąga odpowiednie dane z bazy
+        
                         echo("</tr>");
-                }
+
+                        $conn = new mysqli("mysql-phanteon.alwaysdata.net","phanteon","hasło_testowe","phanteon_test_base");  //Adres do którego się łączę, nazwa użytkownika, hasło, baza danych
+                        $result = $conn->query('SELECT * FROM pracownicy where imie not like "%a"');
+                            echo("<table border=1>");
+                            echo("<th>Id</th>"); //Nazwy Kolumn
+                            echo("<th>Imie</th>");
+                            echo("<th>Dzial</th>");
+                            echo("<th>Zarobki</th>");
+                                while($row=$result->fetch_assoc()){ // wyciąganie danych VVVVVVV
+                                    echo("<tr>");
+                                        echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>"); // wyciąga odpowiednie dane z bazy
+                    
+                                    echo("</tr>");
+
+                                    $conn = new mysqli("mysql-phanteon.alwaysdata.net","phanteon","hasło_testowe","phanteon_test_base");  //Adres do którego się łączę, nazwa użytkownika, hasło, baza danych
+    $result = $conn->query('SELECT * FROM pracownicy where dzial-2');
+        echo("<table border=1>");
+        echo("<th>Id</th>"); //Nazwy Kolumn
+        echo("<th>Imie</th>");
+        echo("<th>Dzial</th>");
+        echo("<th>Zarobki</th>");
+            while($row=$result->fetch_assoc()){ // wyciąganie danych VVVVVVV
+                echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>"); // wyciąga odpowiednie dane z bazy
+
+                echo("</tr>");
                 ?>
-            
