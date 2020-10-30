d908fd74d39030459f43c178365ac15998d743a5
Jojko-Patryk <73522929+Jojko-Patryk@users.noreply.github.com>
2020-10-30 15:35:45 +0100

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,16 +1,7 @@
 <?php
-
-$servername = "mysql-phanteon.alwaysdata.net";
-$username = "phanteon";
-$password = "hasło_testowe";
-$dbanme = "phanteon_test_base";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
+$conn= new mysqli('mysql-phanteon.alwaysdata.net','phanteon','hasło_testowe','phanteon_test_base');
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+    die("connection failed: ".mysqli_connect_error());
 
+}
 ?>
