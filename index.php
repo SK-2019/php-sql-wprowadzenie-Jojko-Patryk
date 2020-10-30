305d134d8502f37047c8872501ab137727555278
Jojko-Patryk <73522929+Jojko-Patryk@users.noreply.github.com>
2020-10-30 14:33:50 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -44,3 +44,20 @@
             }
 
         echo("</table>");
+
+        echo("</table>");
+        $conn = new mysqli("mysql-phanteon.alwaysdata.net","phanteon","hasło_testowe","phanteon_test_base");  
+    $result = $conn->query('SELECT * FROM pracownicy where dzial=2'); // wyświetla tylko pracowników z działu pierwszego i trzeciego
+        echo("<table border=1>");
+        echo("<th>Id</th>"); 
+        echo("<th>Imie</th>");
+        echo("<th>Dzial</th>");
+        echo("<th>Zarobki</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>"); 
+
+                echo("</tr>");
+            }
+
+        echo("</table>");
