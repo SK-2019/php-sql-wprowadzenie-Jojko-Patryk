1626797a5a1dc8f05b7cd5062029a857655031b6
Jojko-Patryk <73522929+Jojko-Patryk@users.noreply.github.com>
2020-10-30 14:32:48 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -13,42 +13,34 @@
                 echo("</tr>");
             }
 
-            $conn = new mysqli("mysql-phanteon.alwaysdata.net","phanteon","hasło_testowe","phanteon_test_base");  //Adres do którego się łączę, nazwa użytkownika, hasło, baza danych
-            $result = $conn->query('SELECT * FROM pracownicy where (dzial=1 or dzial=3)');
-                echo("<table border=1>");
-                echo("<th>Id</th>"); //Nazwy Kolumn
-                echo("<th>Imie</th>");
-                echo("<th>Dzial</th>");
-                echo("<th>Zarobki</th>");
-                    while($row=$result->fetch_assoc()){ // wyciąganie danych VVVVVVV
-                        echo("<tr>");
-                            echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>"); // wyciąga odpowiednie dane z bazy
-        
-                        echo("</tr>");
-
-                        $conn = new mysqli("mysql-phanteon.alwaysdata.net","phanteon","hasło_testowe","phanteon_test_base");  //Adres do którego się łączę, nazwa użytkownika, hasło, baza danych
-                        $result = $conn->query('SELECT * FROM pracownicy where imie not like "%a"');
-                            echo("<table border=1>");
-                            echo("<th>Id</th>"); //Nazwy Kolumn
-                            echo("<th>Imie</th>");
-                            echo("<th>Dzial</th>");
-                            echo("<th>Zarobki</th>");
-                                while($row=$result->fetch_assoc()){ // wyciąganie danych VVVVVVV
-                                    echo("<tr>");
-                                        echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>"); // wyciąga odpowiednie dane z bazy
-                    
-                                    echo("</tr>");
-
-                                    $conn = new mysqli("mysql-phanteon.alwaysdata.net","phanteon","hasło_testowe","phanteon_test_base");  //Adres do którego się łączę, nazwa użytkownika, hasło, baza danych
-    $result = $conn->query('SELECT * FROM pracownicy where dzial-2');
+        echo("</table>");
+        $conn = new mysqli("mysql-phanteon.alwaysdata.net","phanteon","hasło_testowe","phanteon_test_base");  
+    $result = $conn->query('SELECT * FROM pracownicy where imie like "%a"'); // wyświetla tylko kobiety
         echo("<table border=1>");
-        echo("<th>Id</th>"); //Nazwy Kolumn
+        echo("<th>Id</th>"); 
         echo("<th>Imie</th>");
         echo("<th>Dzial</th>");
         echo("<th>Zarobki</th>");
-            while($row=$result->fetch_assoc()){ // wyciąganie danych VVVVVVV
+            while($row=$result->fetch_assoc()){ 
                 echo("<tr>");
-                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>"); // wyciąga odpowiednie dane z bazy
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>"); 
 
                 echo("</tr>");
-                ?>
+            }
+
+        echo("</table>");
+        $conn = new mysqli("mysql-phanteon.alwaysdata.net","phanteon","hasło_testowe","phanteon_test_base");  
+    $result = $conn->query('SELECT * FROM pracownicy where (dzial=1 or dzial=3)'); // wyświetla tylko pracowników z działu pierwszego i trzeciego
+        echo("<table border=1>");
+        echo("<th>Id</th>"); 
+        echo("<th>Imie</th>");
+        echo("<th>Dzial</th>");
+        echo("<th>Zarobki</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>"); 
+
+                echo("</tr>");
+            }
+
+        echo("</table>");
