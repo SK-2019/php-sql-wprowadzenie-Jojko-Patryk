2db7c2182f739ab5963bb9185369297103a789b5
Jojko-Patryk <73522929+Jojko-Patryk@users.noreply.github.com>
2020-11-03 09:44:07 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -16,8 +16,8 @@
         echo("<table border=1>");
         echo("<th>Imie</th>");
         echo("<th>Zarobki</th>");
-        echo("<th>Data_Urodzenia</th>")
-        echo("<th>Nazwa_Działu</th>")
+        echo("<th>Data_Urodzenia</th>");
+        echo("<th>Nazwa_Działu</th>");
             while($row=$result->fetch_assoc()){ 
                 echo("<tr>");
                     echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>"); 
