da6c8c3cebccb221a883213d32b65dec768b2c8e
Jojko-Patryk <73522929+Jojko-Patryk@users.noreply.github.com>
2020-11-06 11:07:34 +0100

    Add files via upload

--- pracownicy.php
+++ pracownicy.php
@@ -22,35 +22,6 @@
 
             echo("</tr>");
 
-    require("connect.php");
-    echo("Pracownicy tylko z działu 2");
-        $result = $conn->query('SELECT imie, zarobki, data_urodzenia, dzial FROM pracownicy WHERE (dzial=2 or dzial=3)');
-            echo("<table border=1>");
-            echo("<th>Imie</th>");
-            echo("<th>Zarobki</th>");
-            echo("<th>Data_Urodzenia</th>");
-            echo("<th>Dział</th>");
-        while($row=$result->fetch_assoc()){ 
-            echo("<tr>");
-                echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["dzial"]."</td>");
-        
-            echo("</tr>");
-
-    require("connect.php");
-    echo("Pracownicy tylko z działu 2");
-        $result = $conn->query('SELECT imie, zarobki, data_urodzenia, dzial FROM pracownicy WHERE (zarobki<30)');
-            echo("<table border=1>");
-            echo("<th>Imie</th>");
-            echo("<th>Zarobki</th>");
-            echo("<th>Data_Urodzenia</th>");
-            echo("<th>Dział</th>");
-        while($row=$result->fetch_assoc()){ 
-            echo("<tr>");
-                echo("<td>".$row["imie"]."</td><td>".$row["zarobki"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["dzial"]."</td>");
-
-            echo("</tr>");        
-
-
 
 
 
