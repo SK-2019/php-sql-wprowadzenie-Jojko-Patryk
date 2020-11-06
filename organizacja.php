72ff115b7926be997ac26a8ba63a1b6fb1db6094
Jojko-Patryk <73522929+Jojko-Patryk@users.noreply.github.com>
2020-11-06 13:51:25 +0100

    Add files via upload

--- organizacja.php
+++ organizacja.php
@@ -84,6 +84,19 @@ $result = $conn->query('SELECT imie, nazwa_dzial FROM `pracownicy`, `organizacja
          } 
     echo("</table>");
 
+require("connect.php");
+echo("<h2>Pracownicy z działu 3 posortowani rosnąco po imieniu</h2>");
+$result = $conn->query('SELECT imie, nazwa_dzial FROM `pracownicy`, `organizacja` WHERE dzial = id_org AND dzial=3 order by imie asc');
+    echo("<table border=1>");
+    echo("<th>Imie</th>");
+    echo("<th>Nazwa_Działu</th>");
+         while($row=$result->fetch_assoc()){ 
+            echo("<tr>");
+                echo("<td>".$row["imie"]."</td><td>".$row["nazwa_dzial"]."</td>"); 
+
+            echo("</tr>");
+    echo("</table>");    
+
     
     
 
