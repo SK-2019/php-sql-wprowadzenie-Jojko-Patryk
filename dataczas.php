eb0333935d58797828d01db377ebd3b7a27857e0
Jojko-Patryk <73522929+Jojko-Patryk@users.noreply.github.com>
2020-11-10 10:05:11 +0100

    Add files via upload

--- dataczas.php
+++ dataczas.php
@@ -25,7 +25,7 @@ echo("<h2>Wiek poszczególnych pracowników</h2>");
 
 require("connect.php");
 echo("<h2>Wiek poszczególnych pracowników z działu serwis</h2>");
-    $result = $conn->query('SELECT *, YEAR(curdate())-YEAR(data_urodzenia) AS wiek FROM pracownicy WHERE dzial=1');
+    $result = $conn->query('SELECT *, YEAR(curdate())-YEAR(data_urodzenia) AS wiek FROM "pracownicy", "organizacja" WHERE dzial=1 AND dzial = id_org');
         echo("<table border=1>");
         echo("<th>Imie</th>");
         echo("<th>Data Urodzenia</th>");
