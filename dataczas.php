ed32076d1bbb8c8683091dde7d9a43a24a2a2792
Jojko-Patryk <73522929+Jojko-Patryk@users.noreply.github.com>
2020-11-10 09:58:05 +0100

    Add files via upload

---
+++ dataczas.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+require("connect.php");
+echo("<h2>Wiek poszczególnych pracowników</h2>");
+    $result = $conn->query('SELECT *, YEAR(curdate())-YEAR(data_urodzenia) AS wiek FROM pracownicy');
+        echo("<table border=1>");
+        echo("<th>Imie</th>");
+        echo("<th>Data Urodzenia>");
+            while($row=$result->fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>".$row["imie"]."</td><td>".$row["data_urodzenia"]."</td>");
+                echo("<tr>");
+                
+            }
+            echo("</table>");
+
+require("connect.php");
+echo("<h2>Wiek poszczególnych pracowników</h2>");
+    $result = $conn->query('SELECT *, YEAR(curdate())-YEAR(data_urodzenia) AS wiek FROM pracownicy WHERE dzial = id_org AND dzial=1');
+        echo("<table border=1>");
+        echo("<th>Imie</th>");
+        echo("<th>Data Urodzenia>");
+        echo("<th>Nazwa Działu<th>");
+            while($row=$result->fetch_assoc()){
+                echo("<tr>");
+                    echo("<td>".$row["imie"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["nazwa_dzial"]);
+                echo("<tr>");
+                
+            }
+            echo("</table>");
+
+
+
+
+
+?>
+
+</body>
+</html>
