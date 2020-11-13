13e376c14e1038ad459358415658313d6ea9e806
Jojko-Patryk <73522929+Jojko-Patryk@users.noreply.github.com>
2020-11-13 11:10:11 +0100

    Add files via upload

--- funkcje.php
+++ funkcje.php
@@ -80,13 +80,43 @@ $result = $conn->query($sql);
 
 require("connect.php");
 $sql = ('SELECT AVG(zarobki) as avg_zarobki FROM pracownicy WHERE (dzial=4)');
-echo("<h2>".$sql."/h2>");
+echo("<h2>".$sql."</h2>");
 $result = $conn->query($sql);
     echo("<table border=1>");
     echo("<th>AVG_Zarobków</th>");
          while($row=$result->fetch_assoc()){ 
             echo("<tr>");
                 echo("<td>".$row["avg_zarobki"]."</td>"); 
+
+            echo("</tr>");
+    
+         }
+         echo("</table>");
+
+require("connect.php");
+$sql = ('SELECT AVG(zarobki) as avg_zarobki FROM pracownicy WHERE (dzial=1 or dzial=2) AND imie not like "%a"');
+echo("<h2>".$sql."</h2>");
+$result = $conn->query($sql);
+    echo("<table border=1>");
+    echo("<th>AVG_Zarobków</th>");
+         while($row=$result->fetch_assoc()){ 
+            echo("<tr>");
+                echo("<td>".$row["avg_zarobki"]."</td>"); 
+
+            echo("</tr>");
+    
+         }
+         echo("</table>");
+
+require("connect.php");
+$sql = ('SELECT COUNT(imie) as liczba_pracownikow FROM pracownicy ')
+echo("<h2>".$sql."</h2>");
+$result = $conn->query($sql);
+    echo("<table border=1>");
+    echo("<th>Liczba_Pracowników</th>");
+         while($row=$result->fetch_assoc()){ 
+            echo("<tr>");
+                echo("<td>".$row["liczba_pracownikow"]."</td>"); 
 
             echo("</tr>");
     
