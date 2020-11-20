9c8df2fb1267dd8ffc3a29d8ea615064e90b488c
Jojko-Patryk <73522929+Jojko-Patryk@users.noreply.github.com>
2020-11-20 15:03:40 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -16,8 +16,20 @@ echo("<h2> Zarobki:".$_POST["zarobki"]."</h2>");
 echo("<h2> Data Urodzenia:".$_POST["data_urodzenia"]."</h2>");
 
 require("connect.php");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
 $sql = sprintf("INSERT INTO pracownicy('imie', 'dzial', 'zarobki', 'data_urodzenia') VALUES(%s,%s,%s,%s)", $_POST['imie'], $_POST['dzial'], $_POST['zarobki'], $_POST['data_urodzenia']);
-echo("<li>".$sql."</li>");
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+  
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  
+  $conn->close();
+
 ?>
 </body>
 </html>
