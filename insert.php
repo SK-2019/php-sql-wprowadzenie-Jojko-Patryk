0500bea0cfc281531caecb022d05dd8ddb0b4dbd
Jojko-Patryk <73522929+Jojko-Patryk@users.noreply.github.com>
2020-11-20 14:51:22 +0100

    up

--- insert.php
+++ insert.php
@@ -6,19 +6,38 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-    require("connect.php");
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-    }
-    
-    $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
-           VALUES (NULL,'Ksawery', 3, 36,'1995-10-21')";
-    
-    
-    $conn->query($sql);
-    
-    $conn->close();
-    ?>
+<?php
+echo("jestes w insert.php <br>");
+echo("<h2> Imie:".$_POST["imie"]."</h2>");
+echo("<h2> Dział:".$_POST["dzial"]."</h2>");
+echo("<h2> Zarobki:".$_POST["zarobki"]."</h2>");
+echo("<h2> Data Urodzenia:".$_POST["data_urodzenia"]."</h2>");
+
+require("connect.php")
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//definiujemy zapytanie $sql
+$sql = "INSERT INTO pracownicy (null, imie, dzial,zarobki,data_urodzenia)
+	      VALUES (
+					null,". 
+					$_POST['imie'], 
+					$_POST['dzial'], 
+					$_POST['zarobki'],
+					$_POST['data_urodzenia']
+				;
+
+//wyświetlamy zapytanie $sql
+echo "<li>". $sql;
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
 </body>
 </html>
