929de2c4a6c2db937a1983f23d61ea38c5890bd7
Jojko-Patryk <73522929+Jojko-Patryk@users.noreply.github.com>
2020-11-20 14:24:23 +0100

    up

--- stuff2.php
+++ stuff2.php
@@ -24,12 +24,10 @@
 <?php
 
 echo("<h2> Imie:".$_POST["imie"]."</h2>");
-echo("<h2> Dział:".$_POST["dzial"]."</h2>");
-echo("<h2> Zarobki:".$_POST["zarobki"]."</h2>");
-echo("<h2> Data Urodzenia:".$_POST["data_urodzenia"]."</h2>");
-
-require("connect.php");
-$sql = sprintf("INSERT INTO pracownicy('imie', 'dzial', 'zarobki', 'data_urodzenia') VALUES(%s,%s,%s,%s)", $_POST['imie'], $_POST['dzial'], $_POST['zarobki'], $_POST['data_urodzenia']);
+echo("<h2> Nazwisko:".$_POST["nazwisko"]."</h2>");
+echo("<h2> Nr.Tel:".$_POST["nr.tel"]."</h2>");
+echo("<h2> Kod Pocztowy:".$_POST["post"]."</h2>");
+echo("<h2> Miasto:".$_POST["city"]."</h2>");
 
 
 
