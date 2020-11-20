bd753b2f8271cc2c45e834a52c9e006393a72f41
Jojko-Patryk <73522929+Jojko-Patryk@users.noreply.github.com>
2020-11-20 11:08:33 +0100

    Update stuff2.php

--- stuff2.php
+++ stuff2.php
@@ -23,7 +23,7 @@ echo("<h2> Data Urodzenia:".$_POST["data_urodzenia"]."</h2>");
 
 
 require("connect.php");
-$sql="INSERT INTO 'pracownicy'('id', 'imie', 'dzial', 'zarobki', 'data_urodzenia') VALUES(null, $_POST['imie'], $_POST['dzial'], $_POST['zarobki'], $_POST['data_urodzenia']");
+$sql="INSERT INTO 'pracownicy'('id', 'imie', 'dzial', 'zarobki', 'data_urodzenia') VALUES(null, $_POST['imie'], $_POST['dzial'], $_POST['zarobki'], $_POST['data_urodzenia']";
         
 
 $conn->query($sql);
