6e9ce222e487ce7333485b147bf415734678048a
Jojko-Patryk <73522929+Jojko-Patryk@users.noreply.github.com>
2020-11-20 14:59:56 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -14,9 +14,8 @@ echo("<h2> Dział:".$_POST["dzial"]."</h2>");
 echo("<h2> Zarobki:".$_POST["zarobki"]."</h2>");
 echo("<h2> Data Urodzenia:".$_POST["data_urodzenia"]."</h2>");
 
-require("connect.php")
+require("connect.php");
 $sql = sprintf("INSERT INTO pracownicy('imie', 'dzial', 'zarobki', 'data_urodzenia') VALUES(%s,%s,%s,%s)", $_POST['imie'], $_POST['dzial'], $_POST['zarobki'], $_POST['data_urodzenia']);
-
 ?>
 </body>
 </html>
