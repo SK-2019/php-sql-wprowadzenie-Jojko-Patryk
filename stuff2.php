44312ef7bf9fee1fe0e21ee6b860f59c9282a839
Jojko-Patryk <73522929+Jojko-Patryk@users.noreply.github.com>
2020-11-20 11:13:56 +0100

    Update stuff2.php

--- stuff2.php
+++ stuff2.php
@@ -20,19 +20,10 @@ echo("<h2> Dział:".$_POST["dzial"]."</h2>");
 echo("<h2> Zarobki:".$_POST["zarobki"]."</h2>");
 echo("<h2> Data Urodzenia:".$_POST["data_urodzenia"]."</h2>");
 
-
+require("connet.php");
 $sql="INSERT INTO 'pracownicy'('id', 'imie', 'dzial', 'zarobki', 'data_urodzenia') VALUES(null, $_POST['imie'], $_POST['dzial'], $_POST['zarobki'], $_POST['data_urodzenia']";
         
 
-$conn->query($sql);
-echo "<h2>".$sql;
-if ($conn->query($sql) === TRUE) {
-    echo "New Record Created Successfully";
-    } else {
-    echo "Error: ".$sql."<br>".$conn->error;    
-    }
-
-$conn->close();
 
 ?>
     
