4f51c9f4b7ec9fb9eb9ecf435387c0a134888c68
Jojko-Patryk <73522929+Jojko-Patryk@users.noreply.github.com>
2020-11-24 09:01:02 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -22,7 +22,7 @@ if ($conn->connect_error) {
   }
   
   $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
-         VALUES (null,'".$_POST['imie']."', 1, 76,'1991-11-21')";
+         VALUES (null,'".$_POST['imie']."', '".$_POST['dzial']"', '".$_POST['zarobki']"', '".$_POST['data_urodzenia']"')";
   
   echo "<li>".$sql;
   
