d46e780701619698b06a2ac341d90908db6d2c16
Jojko-Patryk <73522929+Jojko-Patryk@users.noreply.github.com>
2020-11-24 09:19:29 +0100

    New

---
+++ delete.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style2.css">
+    <title>DELphp</title>
+</head>
+<body>
+<?php
+
+echo("<h2> ID pracownika wybranego do usunięcia:".$_POST["id"]."</h2>"); 
+
+require("connect.php");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+$sql = "DELETE FROM pracownicy WHERE id = $_POST['ID'];";
+
+echo "<h2>".$sql."</h2>";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record Deleted Successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  
+  $conn->close();
+
+
+
+
+
+
+
+
+
+
+?>    
+</body>
+</html>
