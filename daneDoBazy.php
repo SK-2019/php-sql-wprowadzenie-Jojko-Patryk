42446a812a1b3973839c51765b80b585d5489831
Jojko-Patryk <73522929+Jojko-Patryk@users.noreply.github.com>
2020-11-27 15:24:36 +0100

    Update daneDoBazy.php

--- daneDoBazy.php
+++ daneDoBazy.php
@@ -58,7 +58,7 @@ require("connect.php");
                 <td>
 
                     <form action='delete.php' method='POST'>
-                        <input type='number name='id' value='".$row['id_pracownicy']."' hidden></br>
+                        <input type='number name='id' value='".$row['id_pracownicy']."' hidden>
                         <input type='submit' value='Usuń'>
                     </form>
 
