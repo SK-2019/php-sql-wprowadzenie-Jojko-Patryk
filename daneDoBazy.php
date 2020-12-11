b37a5f84f9e547e6cadb2fe20bd3443c3b68baa1
Jojko-Patryk <73522929+Jojko-Patryk@users.noreply.github.com>
2020-12-11 12:28:43 +0100

    Big Update that won't work

--- daneDoBazy.php
+++ daneDoBazy.php
@@ -1,27 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style2.css">
-    <title>DDB</title>
-</head>
-<body>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>CzyToDziała.jpg</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="item colorRed">
+        
+        <h1 class="title">Patryk Jojko</h1>
+      </div>
+      <div class="item colorBlue">
+          <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk">Github</a>
+          <a href="pracownicy.php">Pracownicy</a>
+          <a href="organizacja.php">Pracownicy i Organizacja</a>
+          <a href="funkcje.php">Funkcje Agregujące</a>
+          <a href="dataczas.php">Data i Czas</a>
+          <a href="formularz.html">Zakładka testowa</a>
+          <a href="daneDoBazy.php">Dane Do Bazy</a>
+          <a href="biblioteka.php">Książki</a>
+      </div>
+      <div class="item colorGreen">
 
-<div class=nav>
-    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk">Github</a>
-    |
-    <a href="pracownicy.php">Pracownicy</a>
-    <a href="organizacja.php">Pracownicy i Organizacja</a>
-    <a href="funkcje.php">Funkcje Agregujące</a>
-    <a href="dataczas.php">Data i Czas</a>
-    <a href="formularz.html">Zakładka testowa</a>
-    <a href="daneDoBazy.php">Dane Do Bazy</a>
-    <a href="biblioteka.php">Książki</a>
-    
-
-
-</div>
     <form action="insert.php" method="post">
         <input type="text" name="imie" placeholder="Imię"></br>
         <input type="number" name="dzial" placeholder="Dział"></br>
@@ -75,7 +78,7 @@ require("connect.php");
 
 
 ?>
-
+</div>
 
 </body>
 </html>
--- pracownicy.php
+++ pracownicy.php
@@ -1,26 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Patryk Jojko</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-<div class=nav>
-    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk">Github</a>
-    |
-    <a href="pracownicy.php">Pracownicy</a>
-    <a href="organizacja.php">Pracownicy i Organizacja</a>
-    <a href="funkcje.php">Funkcje Agregujące</a>
-    <a href="dataczas.php">Data i Czas</a>
-    <a href="formularz.html">Zakładka testowa</a>
-    <a href="daneDoBazy.php">Dane Do Bazy</a>
-    <a href="biblioteka.php">Książki</a>
-    
-
-
-</div>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>CzyToDziała.jpg</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="item colorRed">
+        
+        <h1 class="title">Patryk Jojko</h1>
+      </div>
+      <div class="item colorBlue">
+          <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk">Github</a>
+          <a href="pracownicy.php">Pracownicy</a>
+          <a href="organizacja.php">Pracownicy i Organizacja</a>
+          <a href="funkcje.php">Funkcje Agregujące</a>
+          <a href="dataczas.php">Data i Czas</a>
+          <a href="formularz.html">Zakładka testowa</a>
+          <a href="daneDoBazy.php">Dane Do Bazy</a>
+          <a href="biblioteka.php">Książki</a>
+      </div>
+      <div class="item colorGreen">
 
 <?php
 
@@ -120,6 +123,7 @@
 
 
 ?>
+</div>
     
 </body>
 </html>
