e9cb2bdf76ccff14f2b8ab56b6ca88ee7b82f6ac
Jojko-Patryk <73522929+Jojko-Patryk@users.noreply.github.com>
2020-12-11 11:07:29 +0100

    na 99% to nie działa

--- funkcje.php
+++ funkcje.php
@@ -8,14 +8,17 @@
 </head>
 <body>
 <div class=nav>
+    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk">Github</a>
+    |
     <a href="pracownicy.php">Pracownicy</a>
     <a href="organizacja.php">Pracownicy i Organizacja</a>
     <a href="funkcje.php">Funkcje Agregujące</a>
     <a href="dataczas.php">Data i Czas</a>
-    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk">Github</a>
     <a href="formularz.html">Zakładka testowa</a>
     <a href="daneDoBazy.php">Dane Do Bazy</a>
     <a href="biblioteka.php">Książki</a>
+
+    
     
 
 
--- index.php
+++ index.php
@@ -7,15 +7,22 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<div class=nav>
+<div class="container">
+<div class="item colorDG">
+    
+    <h1 class="title">Patryk Jojko</h1>
+</div>
+<div class="item colorWS">
+    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk">Github</a>
     <a href="pracownicy.php">Pracownicy</a>
     <a href="organizacja.php">Pracownicy i Organizacja</a>
     <a href="funkcje.php">Funkcje Agregujące</a>
     <a href="dataczas.php">Data i Czas</a>
-    <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk">Github</a>
     <a href="formularz.html">Zakładka testowa</a>
     <a href="daneDoBazy.php">Dane Do Bazy</a>
     <a href="biblioteka.php">Książki</a>
+</div>
+
     
 
 
