ef6076e941a847e54ba484913ce533f1ffd1aa4a
Jojko-Patryk <jojko.patryk0@gmail.com>
2021-02-05 08:22:10 +0100

    Update wypożycz.php

--- biblioteka/wypożycz.php
+++ biblioteka/wypożycz.php
@@ -78,7 +78,7 @@
 echo("<h2> ID Autora:".$_POST["Autor"]."</h2>");
 require($_SERVER['DOCUMENT_ROOT'] . '/connect2.php');
 
-$sql = ('SELECT * FROM `bibliotekaAT`, `bibliotekaAutor`, `bibliotekaTytuł` where bibliotekaAutor_ID=id_autor and bibliotekaTytul_ID=id_tytuł and bibliotekaAutor_ID='".$_POST['Autor']."'');
+$sql = ("SELECT * FROM `bibliotekaAT`, `bibliotekaAutor`, `bibliotekaTytuł` where bibliotekaAutor_ID=id_autor and bibliotekaTytul_ID=id_tytuł and bibliotekaAutor_ID='".$_POST['Autor']."'");
 
 echo("<h3>".$sql."</h3>");
 $result = $conn->query($sql);
