16c6c3616926ea36f079fd56b0f7c375dc07b257
Jojko-Patryk <jojko.patryk0@gmail.com>
2021-02-23 10:04:53 +0100

    Update connect2.php

--- connect2.php
+++ connect2.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli($_SERVER['address'],$_SERVER['username'], $_SERVER['pass'], $_SERVER['dbname']);
+$conn = new mysqli($_SERVER['address'],$_SERVER['username'], $_SERVER['pass'], $_SERVER['dbname2']);
 if ($conn->connect_error) {
     die("connection failed: ".mysqli_connect_error());
 
