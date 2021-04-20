6800d2b73422e265eb0e57853c86a69d1be44b39
Jojko-Patryk <jojko.patryk0@gmail.com>
2021-04-20 09:50:51 +0200

    chmura robi wjazd

---
+++ assets/menurel.php
@@ -0,0 +1,7 @@
+a href="/pracorgfunc/organizacja.php">
+            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+            Pracownicy i Organizacja
+            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+
+
+          </a>
