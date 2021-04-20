eeb91ba702281905812cfda4bcda6386fd0c3bfd
Jojko-Patryk <jojko.patryk0@gmail.com>
2021-04-20 10:01:12 +0200

    Update header.php

--- assets/header.php
+++ assets/header.php
@@ -7,9 +7,9 @@
         </h1>
 <h1 class="title">
         <a href="https://www.notion.so/SQL-3ccfadeab4b84d7794d7a13966de2547">
-        <img class="nav_icons" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>  
+        <img class="nav_icons" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/chmurka.jpg"/>  
         Notion
-        <img class="nav_icons" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+        <img class="nav_icons" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/chmurka.jpg"/>
 
 
         </h1>
