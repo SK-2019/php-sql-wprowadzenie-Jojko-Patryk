08494aa013b0eb752529f81e7e35e9e1468a10dd
Jojko-Patryk <jojko.patryk0@gmail.com>
2021-04-22 11:15:54 +0200

    Update relations.php

--- assets/relations.php
+++ assets/relations.php
@@ -1,19 +1,19 @@
 <a href="Placeholder">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/chmurka.jpg"/>
+            <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk/main/loading.gif"/>
             Placeholder
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/chmurka.jpg"/>
+            <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk/main/loading.gif"/>
             </a>
 <a href="Placeholder">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/chmurka.jpg"/>
+            <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk/main/loading.gif"/>
             Placeholder
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/chmurka.jpg"/>
+            <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk/main/loading.gif"/>
 
 
           </a>
 <a href="Placeholder">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/chmurka.jpg"/>
+            <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk/main/loading.gif"/>
             Placeholder
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/chmurka.jpg"/>
+            <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk/main/loading.gif"/>
 
 
           </a>
