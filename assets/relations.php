133eaed550ec8fa267efe9cd0ab94418df6dc60d
Jojko-Patryk <jojko.patryk0@gmail.com>
2021-04-26 21:16:24 +0200

    Check Update

--- assets/relations.php
+++ assets/relations.php
@@ -1,18 +1,18 @@
-<a href="Placeholder">
+<a href="../multiple/autor-tytul.php">
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk/main/loading.gif"/>
-            Placeholder
+            Autor-Tytuł
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk/main/loading.gif"/>
             </a>
-<a href="Placeholder">
+<a href="../multiple/szkola.php">
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk/main/loading.gif"/>
-            Placeholder
+            Nauczyciel-Klasa
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk/main/loading.gif"/>
 
 
           </a>
-<a href="Placeholder">
+<a href="../multiple/firma.php">
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk/main/loading.gif"/>
-            Placeholder
+            Producent-Produkt
             <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk/main/loading.gif"/>
 
 
---
+++ multiple/szkola.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/constyle/style.css" />
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+      <div class="item colorRed">
+        
+      <?php include("../assets/header.php") ?>
+        </div>
+        <div class="item colorBlue">
+        <?php include("../assets/menu.php") ?>
+        <?php include("../assets/relations.php") ?>
+      </div>
+      <div class="item colorGreen">
+<?
+require('../connect/connect3.php');
+$sql = "SELECT * FROM B.Klasa";
+echo("<h3>Pracownicy</h3>");
+echo("<li>".$sql);
+$result = $conn->query($sql);
+echo("<table border=1>");
+echo("<th>ID_Klasa</th>");
+echo("<th>Klasa</th>");
+
+while($row=$result->fetch_assoc()){
+    echo("<tr>");
+    echo("<td>".$row['ID_Klasa']."</td><td>".$row['Klasa']."</td>");
+    echo("</tr>");
+}
+echo("</table>");
+
+$sql = "SELECT * FROM B.Nauczyciel";
+echo("<h3>Tytuły</h3>");
+echo("<li>".$sql);
+$result = $conn->query($sql);
+echo("<table border=1>");
+echo("<th>ID_Nau</th>");
+echo("<th>Nauczyciel</th>");
+
+while($row=$result->fetch_assoc()){
+    echo("<tr>");
+    echo("<td>".$row['ID_Nau']."</td><td>".$row['Nauczyciel']."</td>");
+    echo("</tr>");
+}
+echo("</table>");
+
+$sql = 'SELECT * FROM B.Nauczyciel, B.Klasa, B.Both where ID.N = ID_Nau and ID.K = ID_Klasa Order by id asc';
+echo("<h3>Autorzy i Tytuły</h3>");
+echo("<li>".$sql);
+$result = $conn->query($sql);
+echo("<table border=1>");
+echo("<th>id</th>");
+echo("<th>ID.K</th>");
+echo("<th>Klasa</th>");
+echo("<th>ID.N</th>");
+echo("<th>Nauczyciel</th>");
+
+while($row=$result->fetch_assoc()){
+    echo("<tr>");
+    echo("<td>".$row['id']."</td><td>".$row['ID.K']."</td><td>".$row['Klasa']."</td><td>".$row['ID.N']."</td><td>".$row['Nauczyciel']."</td>");
+    echo("</tr>");
+}
+echo("</table>");
+
+?>
+
+</body>
+</html>
