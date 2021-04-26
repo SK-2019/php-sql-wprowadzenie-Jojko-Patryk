54c28739d8d5dafec43fe3f9a71893c3ab89c0e7
Jojko-Patryk <jojko.patryk0@gmail.com>
2021-04-26 21:20:09 +0200

    Update relations.php

--- assets/relations.php
+++ assets/relations.php
@@ -1,19 +1,19 @@
 <a href="../multiple/autor-tytul.php">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk/main/loading.gif"/>
+            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
             Autor-Tytuł
-            <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk/main/loading.gif"/>
+            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
             </a>
 <a href="../multiple/szkola.php">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk/main/loading.gif"/>
+            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
             Nauczyciel-Klasa
-            <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk/main/loading.gif"/>
+            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
 
 
           </a>
 <a href="../multiple/firma.php">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk/main/loading.gif"/>
+            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
             Producent-Produkt
-            <img class="nav_icon" src="https://raw.githubusercontent.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk/main/loading.gif"/>
+            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
 
 
           </a>
