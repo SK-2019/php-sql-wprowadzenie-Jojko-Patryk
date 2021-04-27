ca0c269b9d9a94e399d5f7ffdd08e84bf025fe72
Jojko-Patryk <jojko.patryk0@gmail.com>
2021-04-27 09:37:44 +0200

    Update menu.php

--- assets/menu.php
+++ assets/menu.php
@@ -1,60 +1,60 @@
 
 <a class="ars" href="https://github.com/SK-2019/php-sql-wprowadzenie-Jojko-Patryk">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+            
             Github
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+            
           </a>
           <a class="ars" href="../index.php">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+            
             INDEX
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+            
             </a>
 
 
 
           <a class="ars" href="/pracorgfunc/pracownicy.php">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+            
             Pracownicy
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+            
 
 
           </a>
           <a class="ars" href="/pracorgfunc/organizacja.php">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+            
             Organizacja
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+            
 
 
           </a>
           <a class="ars" href="/pracorgfunc/funkcje.php">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+            
             Funkcje 
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+            
 
 
           </a>
           <a class="ars" href="/pracorgfunc/dataczas.php">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+            
             DataCzas
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+            
 
 
           </a>
           <a class="ars" href="/inne/daneDoBazy.php">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+            
             DaneDoBazy
             
 
 
           </a>
           <a class="ars" href="/biblioteka/biblioteka.php">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+            
             Książki
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+            
           </a>
             
             <a class="ars" href="/flexbox/index.html">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+            
             flexbox
             
 
@@ -64,34 +64,34 @@
             
           
             <a class="ars" href="../multiple/autor-tytul.php">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+            
             Autor|-|Tytul
             
             </a>
             
 <a class="ars" href="../multiple/szkola.php">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+            
             Nauczyciel|-|Klasa
             
 
 
           </a>
 <a class="ars" href="../multiple/firma.php">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+            
             Producent|-|Produkt
             
 
 
           </a>
           <a class="ars" href="../multiple/warsztat.php">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+            
             Mechanik|-|Pojazd
             
 
 
           </a>
           <a class="ars" href="../multiple/pracownik.php">
-            <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-Jojko-Patryk/main/notouchplz.png"/>
+            
             Pracownik|-|Dzial
             
 
