c836629d735e72f48a4f013a3cdaed32ec4fa698
Jojko-Patryk <jojko.patryk0@gmail.com>
2021-04-30 08:25:27 +0200

    Update autor-tytul.php

--- multiple/autor-tytul.php
+++ multiple/autor-tytul.php
@@ -54,7 +54,7 @@ echo("<li>".$sql."</li>");
 table($sql, $conn, "id_tytul", "tytul", 'tytul');
 
 $sql = 'SELECT * FROM autor_tytul, autor, tytul where autor_id = id_autor and tytul_id = id_tytul Order by id asc'
-echo("Autorzy i Tytuly")
+echo("Autorzy i Tytuly");
 echo("<li>".$sql."</li>");
 table($sql, $conn, "nazwisko", "tytul", 'Tytul');
 
